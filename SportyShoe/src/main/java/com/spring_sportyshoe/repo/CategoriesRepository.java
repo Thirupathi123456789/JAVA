74fe82817ea2756e4b5a864650b1b4e370049aad
Thirupathi123456789 <98969164+Thirupathi123456789@users.noreply.github.com>
2022-03-14 20:30:28 +0530

    Delete SportyShoe directory

--- SportyShoe/src/main/java/com/spring_sportyshoe/repo/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring_sportyshoe.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring_sportyshoe.entity.Categories;
-
-public interface CategoriesRepository extends JpaRepository<Categories, Long> {
-
-}
